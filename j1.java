8f355b4e317f283381025bde2e3764fd26154eef
Udit Sharma <121646116+Ud1t-Sharma@users.noreply.github.com>
2023-08-08 23:00:39 +0530

    Add files via upload

---
+++ j1.java
@@ -0,0 +1,8 @@
+class j1{
+    public static void main (String[] args){
+        var a = "WHAT IS OOP???";
+        var b = "OOP stands for Object-Oriented Programming.\nProcedural programming is about writing procedures or methods that perform operations on the data, while object-oriented programming is about creating objects that contain both data and methods.\nObject-oriented programming has several advantages over procedural programming:\nOOP is faster and easier to execute\nOOP provides a clear structure for the programs\nOOP helps to keep the Java code DRY \"Don't Repeat Yourself\", and makes the code easier to maintain, modify and debug\nOOP makes it possible to create full reusable applications with less code and shorter development time";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
