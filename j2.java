6eb4fe6faf78644c8ebd5c6a97f9f14487eb10a5
Udit Sharma <121646116+Ud1t-Sharma@users.noreply.github.com>
2023-08-28 23:30:27 +0530

    Add files via upload

---
+++ j2.java
@@ -0,0 +1,11 @@
+public class j2{
+    int x = 8;
+    float y =0.99f;
+    String ab = "OKAY THIS WAS IT ";
+    public static void main (String [] args) {
+     j2 myObj = new j2();
+        System.out.println("HERE GOES " + myObj.x);
+        System.out.println("HERE GOESSSS " + myObj.y);
+        System.out.println("HERE GOESSSSSS " + myObj.ab);
+    }
+}
